1828bb177ee539f42ee1e5ba5f8e957dee5729f9
Mohammed Alakhras <MabdMAk@gmail.com>
2025-05-02 16:26:03 +0300

    notif

--- src/config/firebase/firebaseAdmin.js
+++ src/config/firebase/firebaseAdmin.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../chat-97789-firebase-adminsdk-fbsvc-97d9e1c06a.json");
+var serviceAccount = require("./chat-97789-firebase-adminsdk-fbsvc-97d9e1c06a.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
